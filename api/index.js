aac161597c75613b2c31d0436da548040d999d85
abdulwahab030910 <abdulwahab030910@gmail.com>
2024-01-31 22:07:12 +0500

    connect to data base

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import  express  from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+
+// const port=process.env.PATH || 3000
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("mongo is connected")
+}).catch(()=>{
+    console.log("error")
+})
 
 const app=express()
 
