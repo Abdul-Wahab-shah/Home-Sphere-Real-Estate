d1edabc1eca2b2b73566c0f27d643968be011514
abdulwahab030910 <abdulwahab030910@gmail.com>
2024-02-02 23:34:11 +0500

    add redux toolkit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-
+import {store} from '../src/reduxToolkit/store.js'
+import { Provider } from 'react-redux';
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <div>
       <App />
     </div>
-  </React.StrictMode>
+  </Provider>
 );
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,12 +1,14 @@
-import { Link,useNavigate } from "react-router-dom";
+import { Link ,  useNavigate } from "react-router-dom";
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { signInStart , signInFailure , signInSuccess } from "../reduxToolkit/user/userSlice";
 
 function SignIn() {
 
 const [formData, setFormData] = useState({});
-const [error, setError] = useState(null);
-const [loading, setLoading] = useState(false);
+const {loading,error}=useSelector((state)=>state.user);
 const navigate=useNavigate();
+const dispatch=useDispatch()
 
   const handleChange = (e) => {
    setFormData({
@@ -17,7 +19,7 @@ const navigate=useNavigate();
   console.log(formData);
 
   const handleSubmit = async(e) => {
-setLoading(true);
+dispatch(signInStart())
     e.preventDefault();
     try{
       const res=await fetch("/api/auth/signin",
@@ -33,19 +35,15 @@ setLoading(true);
     const data=await res.json();
     console.log(data);
     if(data.success === false){
-      setError(data.message);
-      setLoading(false);
+      dispatch(signInFailure(data.message))
       return;
     }
-    setLoading(false);
-    setError(null);
+    dispatch(signInSuccess(data))
     navigate("/");
     }catch(error){
-      setLoading(false);
-setError(error.message);
+      dispatch(signInFailure(error.message))
+
     }
-    
-  
 }
   return (
     <div className="mx-auto max-w-lg p-3">
