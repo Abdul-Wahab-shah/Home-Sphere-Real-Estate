62439152c32bb3b2145cdc965923a61a7bbaadf5
abdulwahab030910 <abdulwahab030910@gmail.com>
2024-02-02 23:33:33 +0500

    add redux toolkit

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -16,20 +16,46 @@ console.error("Error during signup:", error);
   }
 };
 
+// export const signin = async (req, res, next) => {
+//   const { email, password } = req.body;
+
+//   try {
+//     const validUser = await User.findOne({ email });
+
+//     if (!validUser) return next(errorHandler(404, "User not found"));
+
+//     const validPassword = bcrypt.compareSync(password, validUser.password);
+
+//     if (!validPassword) return next(errorHandler(401, "Invalid password"));
+
+//     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+//     const { password: pass, ...rest } = validUser._doc;
+
+//     res
+//     .cookie('access_token', token, { httpOnly: true })
+//     .status(200)
+//     .json({ ...rest, token });
+
+//   } catch (error) {
+//     console.error("Error during signin:", error);
+//     next(error);
+//   }
+// };
 export const signin = async (req, res, next) => {
   const { email, password } = req.body;
- 
   try {
-    const validUser = await User.findOne({ email});
-if(!validUser) return next(errorHandler(404, "User not found"))
-const validPassword= bcrypt.compareSync(password,validUser.password)
-if(!validPassword) return next(errorHandler(401, "Invalid password"))
-const token= jwt.sign({id: validUser._id},process.env.JWT_SECRET,)
-res.cookie('access_token',token,{httpOnly:true}).status(200).json(rest);
-const {password: pass, ...rest}=validUser._doc;
-
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, 'User not found!'));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser
+    res
+      .cookie('access_token', token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
-console.error("Error during signup:", error);
     next(error);
   }
 };
