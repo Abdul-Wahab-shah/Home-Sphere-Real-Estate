b8b89c036fbe8ddf306287fd0c92bb452d5c9e2a
abdulwahab030910 <abdulwahab030910@gmail.com>
2024-02-02 18:04:29 +0500

    ccreate signin route api

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,7 +1,7 @@
 import User from '../models/user.model.js';
 import bcrypt from 'bcrypt';
-// import { errorHandler } from '../utils/error.js';
-// import jwt from 'jsonwebtoken';
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -10,6 +10,24 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json('User created successfully!');
+  } catch (error) {
+console.error("Error during signup:", error);
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+ 
+  try {
+    const validUser = await User.findOne({ email});
+if(!validUser) return next(errorHandler(404, "User not found"))
+const validPassword= bcrypt.compareSync(password,validUser.password)
+if(!validPassword) return next(errorHandler(401, "Invalid password"))
+const token= jwt.sign({id: validUser._id},process.env.JWT_SECRET,)
+res.cookie('access_token',token,{httpOnly:true}).status(200).json(rest);
+const {password: pass, ...rest}=validUser._doc;
+
   } catch (error) {
 console.error("Error during signup:", error);
     next(error);
